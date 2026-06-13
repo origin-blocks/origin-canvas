17e9ad44efcdc3cff53c78f42fddd2a2ae3d532c
Andrew Misplon <amisplon@gmail.com>
2026-06-13 21:12:18 +0200

    Step 4: sidebar widget gap extra-large + tag-cloud margin-top compact (tucks the tag chips up under their Tags heading; emitted inline so it wins over the constrained-layout margin:0 reset)

--- patterns/hidden-sidebar.php
+++ patterns/hidden-sidebar.php
@@ -9,7 +9,7 @@
  */
 
 ?>
-<!-- wp:group {"className":"origin-canvas-sidebar","style":{"spacing":{"blockGap":"var:preset|spacing|medium"}},"fontSize":"small","layout":{"type":"constrained","justifyContent":"left"}} -->
+<!-- wp:group {"className":"origin-canvas-sidebar","style":{"spacing":{"blockGap":"var:preset|spacing|extra-large"}},"fontSize":"small","layout":{"type":"constrained","justifyContent":"left"}} -->
 <div class="wp-block-group origin-canvas-sidebar has-small-font-size"><!-- wp:heading {"level":3,"style":{"typography":{"fontWeight":"600"},"spacing":{"margin":{"bottom":"var:preset|spacing|small"}}},"textColor":"text-heading","fontSize":"regular"} -->
 <h3 class="wp-block-heading has-text-heading-color has-text-color has-regular-font-size" style="margin-bottom:var(--wp--preset--spacing--small);font-weight:600"><?php esc_html_e( 'Recent Posts', 'origin-canvas' ); ?></h3>
 <!-- /wp:heading -->
@@ -26,5 +26,5 @@
 <h3 class="wp-block-heading has-text-heading-color has-text-color has-regular-font-size" style="margin-bottom:var(--wp--preset--spacing--small);font-weight:600"><?php esc_html_e( 'Tags', 'origin-canvas' ); ?></h3>
 <!-- /wp:heading -->
 
-<!-- wp:tag-cloud {"numberOfTags":10,"className":"is-style-origin-canvas-tag-chip","style":{"elements":{"link":{"color":{"text":"var:preset|color|text-heading"}}}},"textColor":"text-heading"} /--></div>
+<!-- wp:tag-cloud {"numberOfTags":10,"className":"is-style-origin-canvas-tag-chip","style":{"elements":{"link":{"color":{"text":"var:preset|color|text-heading"}}},"spacing":{"margin":{"top":"var:preset|spacing|compact"}}},"textColor":"text-heading"} /--></div>
 <!-- /wp:group -->
