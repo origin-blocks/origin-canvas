de5ac679385b52d3c7d69d6c5097de10687146f3
Andrew Misplon <amisplon@gmail.com>
2026-06-18 16:48:08 +0200

    Step 1: Restore blog-loop main margin-bottom to massive

--- patterns/hidden-blog.php
+++ patterns/hidden-blog.php
@@ -11,8 +11,8 @@
 ?>
 <!-- wp:template-part {"slug":"header","theme":"origin-canvas","area":"header"} /-->
 
-<!-- wp:group {"tagName":"main","align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|extra-large","margin":{"top":"var:preset|spacing|colossal","bottom":"var:preset|spacing|colossal"}}},"layout":{"type":"constrained"}} -->
-<main class="wp-block-group alignfull" style="margin-top:var(--wp--preset--spacing--colossal);margin-bottom:var(--wp--preset--spacing--colossal)"><!-- wp:pattern {"slug":"origin-canvas/post-loop-grid"} /--></main>
+<!-- wp:group {"tagName":"main","align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|extra-large","margin":{"top":"var:preset|spacing|colossal","bottom":"var:preset|spacing|massive"}}},"layout":{"type":"constrained"}} -->
+<main class="wp-block-group alignfull" style="margin-top:var(--wp--preset--spacing--colossal);margin-bottom:var(--wp--preset--spacing--massive)"><!-- wp:pattern {"slug":"origin-canvas/post-loop-grid"} /--></main>
 <!-- /wp:group -->
 
 <!-- wp:template-part {"slug":"footer","theme":"origin-canvas","area":"footer"} /-->
